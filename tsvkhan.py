448dfa934c27f04ab52be2226122c7e65509f02d
Ivan Savov <ivan.savov@gmail.com>
2020-08-10 13:33:55 -0400

    Added TODO + note about filtering ALL unlisted nodes in future

--- tsvkhan.py
+++ tsvkhan.py
@@ -231,8 +231,16 @@ def clean_tsv_row(row):
 def _recurse_create(node, tree_dict, topics_by_slug, lang="en", onlylisted=True):
     """
     Main tree-building function that takes the rows from the TSV data and makes
-    a KhanNode tree out of them.
+    a KhanNode tree out of them. By default we want to process only nodes with
+    `listed=True` (onlylisted=True). Use onlylisted=False only for debugging.
     """
+    ## Aug 10 2020   TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+    ## Based on recommendation from the KA team, only nodes with listerd=True
+    ## should be included in the Kolibri channels, which can be accomplised using
+    ## the following two lines:
+    # if onlylisted and (node['listed'] == False or node['listed'] == None):
+    #     return None   # we want to keep only nodes with `listed=True`
+    ##   /TODO /TODO /TODO /TODO /TODO /TODO /TODO /TODO /TODO /TODO /TODO /TODO
 
     # Title info comes form different place if `en` vs. translated trees
     title = node['original_title'] if lang=='en' else node['translated_title']
@@ -273,6 +281,12 @@ def _recurse_create(node, tree_dict, topics_by_slug, lang="en", onlylisted=True)
     elif node["kind"] in TOPIC_LIKE_KINDS or node["kind"] == "Root":
         if onlylisted and (node['listed'] == False or node['listed'] == None):
             return None   # we keep only topics with `listed=True` in the tree
+            ## see TODO TODO TODO section above for how to enable filtering for
+            ## all nodes instead of just topic nodes. The global unlisted ignore
+            ## has was not turned on in order to minimize the logical changes
+            ## between the old API and the new TSV exports API (tsvkhan branch).
+            ## Filtering out only unlisted topic nodes reproduces the same trees
+            ## as we were previously getting from the v2 JSON API.
         khan_node = KhanTopic(
             id=node["slug"],   # set topic id to slug (used for source_id later)
             title=title,
